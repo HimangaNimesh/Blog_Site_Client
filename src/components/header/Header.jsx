87597234776bf09cdb4abd4b4e1986f148824545
himanganimesh2 <himanganimesh2@gmail.com>
2023-04-26 00:03:02 +0530

    header finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import './header.css'
 export default function Header() {
   return (
     <div className='header'> 
-        header
+        <div className='headerTitles'>
+            <span className='headerTitlesSm'>React & Node</span>
+            <span className='headerTitlesLg'>Blog</span>
+        </div>
+        <img className='headerImg' src="https://images.pexels.com/photos/6045035/pexels-photo-6045035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
     </div>
   )
 }
